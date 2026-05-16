bd2f38087c6a16d33c0389f1c9fd5247ba313f41
Shristy <shristy712@gmail.com>
2026-05-17 01:07:15 +0530

    Add SFML and standard library includes

--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
-
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include "PlayerCar.h"
+#include "EnemyCar.h"
