101c310a01c23b37eea27843a5601a074fe82a04
Shristy <shristy712@gmail.com>
2026-05-17 01:07:40 +0530

    Set up game window and load asset textures
    Initialize the game window and load textures for cars and road.

--- main.cpp
+++ main.cpp
@@ -6,3 +6,14 @@
 #include <string>
 #include "PlayerCar.h"
 #include "EnemyCar.h"
+
+int main() {
+ RenderWindow window(VideoMode(800, 600), "Car Game");
+ Texture roadTex, playerTex, red1, red2, yellow1, yellow2, yellow3;
+ playerTex.loadFromFile("Assets/WhiteCar.png");
+ red1.loadFromFile("Assets/RedCar1.png");
+ red2.loadFromFile("Assets/RedCar2.png");
+ yellow1.loadFromFile("Assets/YellowCar1.png");
+ yellow2.loadFromFile("Assets/YellowCar2.png");
+ yellow3.loadFromFile("Assets/YellowCar3.png");
+ roadTex.loadFromFile("Assets/road.png");
