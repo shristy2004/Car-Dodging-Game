32f233d9033d7908d03822a7af9a1f27c8ad964b
Shristy <shristy712@gmail.com>
2026-05-18 00:24:15 +0530

    Define Car class with pure virtual move method

--- Car.h
+++ Car.h
@@ -2,3 +2,12 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 using namespace sf;
+class Car {
+protected:
+ Sprite sprite;
+public:
+ virtual void move(float dt) = 0;
+ Sprite& getSprite() {
+ return sprite;
+ }
+}
