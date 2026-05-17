64931d9287316abbfa402669a66282ae5bca9375
Shristy <shristy712@gmail.com>
2026-05-18 00:25:25 +0530

    Set up PlayerCar sprite properties
    Initialize sprite with texture and set its scale and position.

--- PlayerCar.h
+++ PlayerCar.h
@@ -9,3 +9,12 @@ private:
  float y;
 public:
  PlayerCar(Texture& texture) {
+sprite.setTexture(texture);
+ sprite.setScale(0.35f, 0.35f);
+ FloatRect bounds = sprite.getLocalBounds();
+ sprite.setOrigin(bounds.width / 2, bounds.height / 2);
+ lanes = {150, 400, 650};
+ currentLane = 1;
+ y = 500;
+ sprite.setPosition(lanes[currentLane], y);
+ 
