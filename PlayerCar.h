94c9433a6578a507f10df71272877cf3c535ca06
Shristy <shristy712@gmail.com>
2026-05-18 00:25:05 +0530

    Add PlayerCar class definition with member variables

--- PlayerCar.h
+++ PlayerCar.h
@@ -2,3 +2,10 @@
 #pragma once
 #include "Car.h"
 #include <vector>
+class PlayerCar : public Car {
+private:
+ std::vector<float> lanes;
+ int currentLane;
+ float y;
+public:
+ PlayerCar(Texture& texture) {
